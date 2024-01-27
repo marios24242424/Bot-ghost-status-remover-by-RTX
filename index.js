708bdaa1e1dcfb874828eb92c21d527d5de5fd6a
marios24242424 <157917641+marios24242424@users.noreply.github.com>
2024-01-27 21:26:41 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Παίζει illegal Roleplay"];
 
 
 let currentIndex = 0;
